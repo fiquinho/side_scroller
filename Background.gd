83c8857f0e7a21254a4dcc9906c6b3365f0cea02
Ale <charlie14_1939@hotmail.com>
2023-07-17 15:37:41 -0300

    Add neon lights background

---
+++ Background.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= delta * 400
+	if position.x <= -1920:
+		position.x = 0
+	pass
